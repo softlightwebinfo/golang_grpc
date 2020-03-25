b87db4476831166273ec69f89e824d29e99bc767
Rafa Gonzalez <softlightwebinfo@gmail.com>
2020-03-25 11:07:11 +0100

    INITIAL

---
+++ go.mod
@@ -0,0 +1,12 @@
+module chat_grpc
+
+go 1.12
+
+require (
+	github.com/gin-gonic/gin v1.6.1
+	github.com/golang/protobuf v1.3.5
+	golang.org/x/lint v0.0.0-20190313153728-d0100b6bd8b3 // indirect
+	golang.org/x/tools v0.0.0-20190524140312-2c0ae7006135 // indirect
+	google.golang.org/grpc v1.28.0
+	honnef.co/go/tools v0.0.0-20190523083050-ea95bdfd59fc // indirect
+)
---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"net"
+
+	"chat_grpc/proto"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+type server struct{}
+
+func (s *server) Add(ctx context.Context, request *proto.Request) (*proto.Response, error) {
+	a, b := request.GetA(), request.GetB()
+	result := a + b
+
+	return &proto.Response{
+		Result: result,
+	}, nil
+}
+
+func (s *server) Multiply(ctx context.Context, request *proto.Request) (*proto.Response, error) {
+	a, b := request.GetA(), request.GetB()
+	result := a * b
+
+	return &proto.Response{
+		Result: result,
+	}, nil
+}
+
+func main() {
+	listener, err := net.Listen("tcp", ":4040")
+	if err != nil {
+		panic(err)
+	}
+	srv := grpc.NewServer()
+	proto.RegisterAddServiceServer(srv, &server{})
+	reflection.Register(srv)
+
+	if e := srv.Serve(listener); e != nil {
+		panic(e)
+	}
+}
